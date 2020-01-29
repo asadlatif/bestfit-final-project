0984e9c94d1f8d88b808348ce0cab8e8c693ad55
asadlatif <asadlatif49@gmail.com>
2020-01-29 15:56:27 +0500

    admin model created

--- online_quiz/admin.py
+++ online_quiz/admin.py
@@ -2,22 +2,24 @@ from django.contrib import admin
 from .models import Test
 from .models import Profile
 from .models import Feedbackk
-from django.contrib.auth.models import Group
+# from django.contrib.auth.models import Group
 from django.urls import path
 #from django.utils.html import format_html 
 
 class TestAdmin(admin.ModelAdmin):
     list_display = ('question', "option_1", "option_2", 'option_3', 'option_4', "answer")
 
-class questionAdmin(admin.ModelAdmin):
-    list_filter = ('qustion', 'answer')
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'location', 'gender')
+# class questionAdmin(admin.ModelAdmin):
+#     list_filter = ('qustion', 'answer')
 
 admin.site.register(Test, TestAdmin)
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Feedbackk)
-admin.site.site_header = 'BestFit Organization'
-admin.site.index_title = 'BestFit Administration'
-admin.site.site_title = 'Admin pannel of bestfit organization'
-admin.site.index_template = 'admin/index_template.html'
+# admin.site.site_header = 'BestFit Organization'
+# admin.site.index_title = 'BestFit Administration'
+# admin.site.site_title = 'Admin pannel of bestfit organization'
+# admin.site.index_template = 'admin/index_template.html'
 
 
